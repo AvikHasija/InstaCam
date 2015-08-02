d57f8ceb0d27e098d64853e2fd642ba6169e195f
AvikHasija <avikhasija@gmail.com>
2015-08-02 01:02:13 -0400

    Eighth Commit

--- app/src/main/java/com/example/avikhasija/instacam/LoginActivity.java
+++ app/src/main/java/com/example/avikhasija/instacam/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.avikhasija.instacam;
 
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -7,9 +8,17 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.facebook.HttpMethod;
+import com.facebook.Request;
+import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.SessionState;
 import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphObject;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+
+import java.util.List;
 
 
 public class LoginActivity extends ActionBarActivity {
@@ -23,6 +32,9 @@ public class LoginActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        LoginButton loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_birthday");
+
         mUiLifecycleHelper = new UiLifecycleHelper(this, new Session.StatusCallback() {
             @Override
             public void call(Session session, SessionState sessionState, Exception e) {
@@ -33,12 +45,53 @@ public class LoginActivity extends ActionBarActivity {
         mUiLifecycleHelper.onCreate(savedInstanceState);
     }
 
-    private void onSessionStateChanged(Session session, SessionState sessionState, Exception e){
+    private void onSessionStateChanged(final Session session, SessionState sessionState, Exception e){
         if (sessionState.isOpened()){
             //Session is now open
 
-            Intent i = new Intent(this, MainActivity.class);
-            startActivity(i);
+            List<String> permissions = Session.getActiveSession().getPermissions();
+
+            boolean hasBirthdayPermission = false;
+            for (String permission:permissions){
+                if (permission.equals("user_birthday")){
+                    hasBirthdayPermission = true;
+                }
+            }
+
+            if (!hasBirthdayPermission){
+                Session.NewPermissionsRequest permissionsRequest = new Session.NewPermissionsRequest(this, "user_birthday");
+                session.requestNewReadPermissions(permissionsRequest);
+                return;
+            }
+
+            Bundle parameters = new Bundle();
+            parameters.putString("fields", "picture,first_name,last_name,birthday");
+
+            Request request = new Request(session, "/me", parameters, HttpMethod.GET, new Request.Callback() {
+                @Override
+                public void onCompleted(Response response) {
+                    if (session == Session.getActiveSession()){
+                        if (response.getGraphObject() != null){
+                            GraphObject graphObject = response.getGraphObject();
+                            String firstName = (String) graphObject.getProperty("first_name");
+
+                            String url = (String) graphObject.getPropertyAs("picture", GraphObject.class)
+                                    .getPropertyAs("data", GraphObject.class)
+                                    .getProperty("url");
+
+                            Log.d(TAG, "URL is " +url);
+
+                            Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(i);
+                        }
+                    }
+                    if (response.getError() != null){
+                        Log.d(TAG, "Error is " + response.getError());
+                    }
+                }
+            });
+
+            request.executeAsync();
         }
         else {
 
